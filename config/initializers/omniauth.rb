58a7a1c5b2719999b9b0fac2faf34f0a599495d9
Jeremy Prevost <jprevost@mit.edu>
2017-11-02 11:43:31 -0400

    Update SP Metadata to meet MIT expectations
    What:
    
    * add MIT required entity_id
    * remove MIT unexpected required attributes
    * renamed some ENV settings to make the easier to understand

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,6 +6,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
            idp_sso_target_url: idp_metadata[:idp_sso_target_url],
            idp_cert: idp_metadata[:idp_cert],
            idp_cert_fingerprint: idp_metadata[:idp_cert_fingerprint],
-           certificate: Base64.strict_decode64(ENV['CERTIFICATE']),
-           private_key: Base64.strict_decode64(ENV['PRIVATE_KEY'])
+           certificate: Base64.strict_decode64(ENV['SP_CERTIFICATE']),
+           private_key: Base64.strict_decode64(ENV['SP_PRIVATE_KEY']),
+           request_attributes: {},
+           issuer: ENV['SP_ENTITY_ID']
 end
