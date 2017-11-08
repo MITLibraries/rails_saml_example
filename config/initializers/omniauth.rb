a42df5d8dacd01ce710c21d70807172dd63ffcee
Jeremy Prevost <jprevost@mit.edu>
2017-11-08 11:45:54 -0500

    Configure authn signing

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@
 idp_metadata_parser = OneLogin::RubySaml::IdpMetadataParser.new
 idp_metadata = idp_metadata_parser.parse_remote_to_hash(
   ENV['IDP_METADATA_URL'],
-  false, # validate cert
+  true, # validate cert
   entity_id: ENV['IDP_ENTITY_ID']
 )
 
@@ -15,5 +15,10 @@ Rails.application.config.middleware.use OmniAuth::Builder do
            certificate: Base64.strict_decode64(ENV['SP_CERTIFICATE']),
            private_key: Base64.strict_decode64(ENV['SP_PRIVATE_KEY']),
            request_attributes: {},
-           issuer: ENV['SP_ENTITY_ID']
+           issuer: ENV['SP_ENTITY_ID'],
+           security: { authn_requests_signed: true,
+                       want_assertions_signed: true,
+                       metadata_signed: true,
+                       digest_method: XMLSecurity::Document::SHA256,
+                       signature_method: XMLSecurity::Document::RSA_SHA256 }
 end
